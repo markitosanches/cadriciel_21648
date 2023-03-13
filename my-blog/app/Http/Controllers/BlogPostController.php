15e8f6191df3ed09daa57f855605a18a8d8e9d83
Marcos <markitosanches@gmail.com>
2023-03-13 14:55:34 -0400

    seance 5

---
+++ my-blog/app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class BlogPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //pdo->query(SELECT * FROM blog_posts)->fetchAll();
+        $blog = BlogPost::all();
+
+        return $blog;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BlogPost $blogPost)
+    {
+        //SELECT * FROM blog_posts WHERE id = ?
+        //PDO->prepare(SELECT * FROM blog_posts WHERE id = ?)
+        //->execute(array($blogPost))
+        //$blogPost = fetch()
+        return $blogPost;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BlogPost $blogPost)
+    {
+        //
+    }
+}
